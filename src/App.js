83d49736d6c03c6ae218eb2e4e388e224b8d02a0
Abu Sayeed <sayeed@pop-os.localdomain>
2023-04-08 10:55:38 +0600

    installed react router dom and created Router provider router.js file and Layout Main.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './Router/router';
 
 function App() {
   return (
     <div >
       <h5>This is a test</h5>
       <button className='btn btn-primary'>test</button>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
