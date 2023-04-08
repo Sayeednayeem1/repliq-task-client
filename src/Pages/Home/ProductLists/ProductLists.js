c90666864de6baf6a4943e4f05fad4fc80f06c5e
Abu Sayeed <sayeed@pop-os.localdomain>
2023-04-08 20:24:17 +0600

    created ProductList component to show products card

--- src/Pages/Home/ProductLists/ProductLists.js
+++ src/Pages/Home/ProductLists/ProductLists.js
@@ -1,9 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const ProductLists = () => {
+    const { data: products = [], isLoading } = useQuery({
+        queryKey: ['products'],
+        queryFn: () => fetch('products.json')
+            .then(res => res.json())
+    })
     return (
         <div>
-            <h1>This is Product lists</h1>
+            <h1>products are here</h1>
         </div>
     );
 };
